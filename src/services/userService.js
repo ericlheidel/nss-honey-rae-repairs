091d164443423f7815441c8b008d04df33a0feca
Eric Heidel <ericleeheidel@yahoo.com>
2024-01-04 09:16:35 -0600

    Begin Ch. 12

--- src/services/userService.js
+++ src/services/userService.js
@@ -7,3 +7,18 @@ export const getStaffUsers = () => {
 	return fetch(`http://localhost:8000/users?isStaff=true`).then(
 		(res) => res.json())
 }
+export const getUserByEmail = (email) => {
+  return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+    res.json()
+  )
+}
+
+export const createUser = (customer) => {
+  return fetch("http://localhost:8088/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  }).then((res) => res.json())
+}
